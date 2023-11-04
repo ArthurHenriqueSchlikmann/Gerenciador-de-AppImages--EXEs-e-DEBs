6ec2d5eb55a49452ed5e73af55c6c59ecea9fdd5
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2023-11-04 10:54:31 -0300

    v2

--- Gerenciador_de_Debs_AppImages_e_EXEs/src/main/java/com/mycompany/gerenciador_de_debs_appimages_e_exes/PrimaryController.java
+++ Gerenciador_de_Debs_AppImages_e_EXEs/src/main/java/com/mycompany/gerenciador_de_debs_appimages_e_exes/PrimaryController.java
@@ -2,11 +2,19 @@ package com.mycompany.gerenciador_de_debs_appimages_e_exes;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Menu;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private MenuItem Web;
+    private MenuBar barra;
+    private Menu file;
+    
+    @FXML
+    private void ViraWeb() throws IOException {
+        
     }
 }
