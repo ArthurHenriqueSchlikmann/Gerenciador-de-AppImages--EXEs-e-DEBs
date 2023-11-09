59b0a34af67af4501040a999b7a4329ab89a7180
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2023-11-09 18:21:19 -0300

    v9

--- Gerenciador_de_Debs_AppImages_e_EXEs/src/main/java/com/mycompany/gerenciador_de_debs_appimages_e_exes/WebController.java
+++ Gerenciador_de_Debs_AppImages_e_EXEs/src/main/java/com/mycompany/gerenciador_de_debs_appimages_e_exes/WebController.java
@@ -4,9 +4,13 @@
  */
 package com.mycompany.gerenciador_de_debs_appimages_e_exes;
 
+import java.io.IOException;
 import java.net.URL;
+import javafx.scene.layout.VBox;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
@@ -15,6 +19,10 @@ import javafx.fxml.Initializable;
  */
 public class WebController implements Initializable {
 
+    @FXML
+    private VBox caixa;
+    private Button sair;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
